9e3be3cab914352f56c5351adbf7fc7296ae8f86
SALMA EZZAT <163161326+ezzatsalma@users.noreply.github.com>
2024-04-03 16:57:26 +0500

    programming

---
+++ index.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+
+import inquirer from "inquirer";
+const randomNumber=Math.floor(Math.random()*10);
+const answers= await inquirer.prompt([
+    {
+
+    name:"number_guessed_by_user",
+    type: "number",
+    message:"guess a number b/w 0-10",
+}
+]);
+
+console.log(answers);
+
+if(answers.number_guessed_by_user===randomNumber){
+    console.log("you guessed the right number");
+
+}
+else{
+    console.log("you guessed the wrong number");
+}
+
+
+
